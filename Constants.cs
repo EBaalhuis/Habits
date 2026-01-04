092e552af6a4db4a441d634c17668b0afcd25f1c
Erik Baalhuis <ebbaalhuis@gmail.com>
2026-01-04 14:13:49 +0100

    Add Constants.cs with sqlite constants

---
+++ Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace Habits;
+
+public static class Constants
+{
+    public const string DatabaseFilename = "HabitsSQLite.db3";
+
+    public const SQLite.SQLiteOpenFlags Flags =
+        SQLite.SQLiteOpenFlags.ReadWrite |
+        // create the database if it doesn't exist
+        SQLite.SQLiteOpenFlags.Create |
+        // enable multi-threaded database access
+        SQLite.SQLiteOpenFlags.SharedCache;
+
+    public static string DatabasePath => Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
+}
