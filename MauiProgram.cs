0892460dcabbbb95354d7d91c442ff39e8371ff1
Erik Baalhuis <ebbaalhuis@gmail.com>
2026-01-04 14:33:34 +0100

    Revert "Register DataAccess for DI"
    This reverts commit 4d759905f01cee804f9c8e1cf047a610d8f83af2.

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,5 +1,4 @@
-﻿using Habits.Data;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 
 namespace Habits;
 
@@ -15,8 +14,6 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
-
-        builder.Services.AddSingleton<DataAccess>();
 
 #if DEBUG
 		builder.Logging.AddDebug();
