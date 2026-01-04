4d759905f01cee804f9c8e1cf047a610d8f83af2
Erik Baalhuis <ebbaalhuis@gmail.com>
2026-01-04 14:18:36 +0100

    Register DataAccess for DI

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Habits.Data;
+using Microsoft.Extensions.Logging;
 
 namespace Habits;
 
@@ -14,6 +15,8 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
+
+        builder.Services.AddSingleton<DataAccess>();
 
 #if DEBUG
 		builder.Logging.AddDebug();
