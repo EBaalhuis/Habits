7b3bcfca915286012247eb0c16c8a7af5f53535b
Erik Baalhuis <ebbaalhuis@gmail.com>
2026-01-04 14:13:26 +0100

    Add HabitEntry model

---
+++ Models/HabitEntry.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace Habits.Models;
+
+public class HabitEntry
+{
+    [PrimaryKey, AutoIncrement]
+    public int ID { get; set; }
+    public string? Habit { get; set; }
+    public DateTime Date { get; set; }
+    public bool Enabled { get; set; }
+}
