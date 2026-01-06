7d9a296980a6cd3462f36169ef2ad03205eae11a
Erik Baalhuis <ebbaalhuis@gmail.com>
2026-01-06 19:37:24 +0100

    Make Habit implement IComparable<Habit>

--- Models/Habit.cs
+++ Models/Habit.cs
@@ -2,9 +2,15 @@
 
 namespace Habits.Models;
 
-public class Habit
+public class Habit : IComparable<Habit>
 {
     [PrimaryKey, AutoIncrement]
     public int ID { get; set; }
     public string? Name { get; set; }
+
+    public int CompareTo(Habit? other)
+    {
+        if (other is null) return 1;
+        return ID.CompareTo(other.ID);
+    }
 }
